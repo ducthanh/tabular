9967be4b5152debbfc5981a747f78a7cae32e531
Thanh Duc Nguyen <thanh.nguyenduc@hellopay.com>
2016-06-14 11:33:10 +0700

    update remain files

--- cssblock.rb
+++ cssblock.rb
@@ -83,3 +83,16 @@ end
 puts text_transfer('the quick brown fox jumps over the lazy dog', 3)
 
 puts "HAPPY NEW YEARS 2016"
+
+class Widget
+  def overlapping? (other)
+    x1, y1  = @screen_x, @screen_y
+    x2, y2 = other.screen_coordinates
+  end
+
+  protected
+
+  def screen_coordinates
+    [@screen_x, @screen_y]
+  end
+end
